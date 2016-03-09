40a9849781b3eb7e6f5d9ac4f224429c7edfe70a
JD Bothma <jbothma@gmail.com>
2016-03-09 11:43:00 +0200

    Add initial PHP file and plugin metadata

---
+++ health-e-story-db.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Health-e Story DB
+Plugin URI: http://github.com/code4sa/health-e-story-db
+Version: 0.1
+Author: <a href="http://code4sa.org/">Code4SA</a>
+Description: Allows creation of news story Syndications from the edit interface of the given story. Also provides a DB Export button.
+*/
+?>
