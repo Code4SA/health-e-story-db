912d9c2169e15d6aa02cccb5bc0d5fb7347a2b32
JD Bothma <jbothma@gmail.com>
2016-03-09 17:09:42 +0200

    found the right names and things to add action to insert form

--- health-e-story-db.php
+++ health-e-story-db.php
@@ -7,10 +7,23 @@ Author: <a href="http://code4sa.org/">Code4SA</a>
 Description: Allows creation of news story Syndications from the edit interface of the given story. Also provides a DB Export button.
 */
 
-add_action( 'post_edit_form_tag', 'post_edit_form_tag' );
+//add_action('post_edit_form_tag', 'debug');
+add_action( 'pods_meta_meta_post_radio_publications', 'post_edit_form_tag', 10, 3);
+add_action( 'pods_meta_meta_post_print_syndications', 'post_edit_form_tag', 10, 3 );
+add_action( 'pods_meta_meta_post_tv_broadcasts', 'post_edit_form_tag', 10, 3 );
+add_action( 'pods_meta_meta_post_online_publications', 'post_edit_form_tag', 10, 3 );
 
-function post_edit_form_tag( ) {
-    echo ' hello world ';
+function debug() {
+  global $wp_filter;
+  ob_start();
+  var_dump($wp_filter);
+  $result = ob_get_clean();
+  echo ' data-vardump="' . $result . '"';
+}
+
+function post_edit_form_tag($post, $field, $pod) {
+  //
+  echo '<tr><td>DEADBEEF_' . $field['name'] . '</td></tr>';
 }
 
 ?>
